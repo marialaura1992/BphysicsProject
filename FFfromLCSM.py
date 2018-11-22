fixed FF calculation

FFfromLCSM.py
@@ -15,11 +15,11 @@ from matplotlib import rc
 Central_Coefficients = np.array([
     [0.37, -1.37, 0.13], #A0
     [0.30, 0.39, 1.19],  #A1
-    [0.27, 0.53, 0.48],  #A2
+    [0.27, 0.53, 0.48],  #A12
     [0.38, -1.17, 2.42], #V
     [0.31, -1.01, 1.53], #T1
     [0.31, 0.50, 1.61],  #T2
-    [0.67, 1.32, 3.82]])  #T2
+    [0.67, 1.32, 3.82]])  #T23
 
 
 #Coefficients uncertainties
@@ -27,17 +27,18 @@ Central_Coefficients = np.array([
 Delta_Coefficients = np.array([
     [0.03, 0.26, 1.63],  #A0
     [0.03, 0.19, 1.03],  #A1
-    [0.02, 0.13, 0.66],  #A2
+    [0.02, 0.13, 0.66],  #A12
     [0.03, 0.26, 1.53],  #V
     [0.03, 0.19, 1.64],  #T1
     [0.03, 0.17, 0.80],  #T2
-    [0.06, 0.22, 2.20]]) #T3
+    [0.06, 0.22, 2.20]]) #23
 
 
 
 Masses = {'m_B' : 5.279,
           'm_K*' : 0.895,
-          'm_R' : [5.366, 5.415, 5.829]
+          'm_R' : [5.366, 5.829, 5.829, \
+                   5.415, 5.415, 5.829, 5.829]
 }
 
 
@@ -54,16 +55,10 @@ t0 = t('+') * (1 - np.sqrt(1 - (t('-')/t('+'))))
 #q stands for q**2!!!
 
 def P(q):
-    res = [Masses['m_R'][1]]*2
-    res1 = [Masses['m_R'][2]]*4
-    m = [[Masses['m_R'][0]]]
-    m.append(res)
-    m.append(res1)
-    m = [y for x in m for y in x]
-    res2 = []
-    for i in range(len(m)):
-        res2.append((1 - (q/m[i]**2))**-1)
-    return res2
+    res = np.array([])
+    for i in range(len(Masses['m_R'])):
+        res = np.append(res, (1 - (q/Masses['m_R'][i]**2))**-1)
+    return res
 
 
 def z(q):
@@ -72,47 +67,55 @@ def z(q):
     return factor1/factor2
 
 
-def FF(q, value):
-    if value == 'central':
+#value can be 'central' or 'limit' while
+#sign can be '-' for the central value
+#'up' for positive uncertainty
+#'down' for negative uncertainty
+
+def FF(q, value, sign):
+    if value == 'central' and sign == '-':
         Coefficients = Central_Coefficients
-    if value == 'limit':
-        Coefficients = Delta_Coefficients
-    res = np.array([])
-    for i in range(7):
-        res1 = 0
-        for j in range(len(Coefficients[0])):
-            res1 += Coefficients[i][j] * \
-                    (z(q) - z(0))**j
-        res = np.append(res, P(q)[i] * res1)
+    elif value == 'limit' and sign == 'up':
+        Coefficients = Central_Coefficients +\
+                       Delta_Coefficients
+    elif value == 'limit' and sign == 'down':
+        Coefficients = Central_Coefficients -\
+                       Delta_Coefficients
+    else:
+        return('Error')
+    res = np.zeros((len(q), len(Coefficients)))
+    for k in range(len(q)):
+        for i in range(len(Coefficients)):
+            res1 = 0
+            for j in range(len(Coefficients[0])):
+                res1 += Coefficients[i][j] * \
+                        (z(q[k]) - z(0))**j
+            res[k][i] = P(q[k])[i] * res1
     return res
-
-
-def MaxMinFF(q):
-    MaxVal = FF(0, 'central') + FF(0, 'limit')
-    MinVal = FF(0, 'central') - FF(0, 'limit')
-    return(MaxVal, MinVal)
-
-
-def FF_central(q):
-    return (FF(q, 'central').tolist())
-
-
-def FF_MaxMin(q):
-    return (MaxMin(q).tolist())
-
 
 q = np.arange(0, 20, 0.1)
 
+
+#print('in 0 ', FF([0], 'central', '-'), '\n')
+
+#print('in 1 ', FF([1], 'central', '-'), '\n')
+
+#print('in 2 ', FF([2], 'central', '-'), '\n')
+
+#print(FF(19.9, 'central', '-'), '\n')
+
+#print(FF(q, 'central', '-'))
+#print(len(FF(q, 'central', '-')))
+#print(FF(q, 'central', '-').reshape(len(q), len(Central_Coefficients))[0])
+ff = ['V', 'A1', 'A12', 'T1', 'T2', 'T12']
+
+
 rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
-'''
-plt.plot(q, FF(q, 'central')[0], 'b-', label = 'LCSM + Lattice')
+plt.plot(q, FF(q, 'central', '-')[:,6], 'b--', label = 'LCSM + Lattice')
 plt.xlabel('$q^2$')
-plt.ylabel('$A_0(q^2)$')
+plt.ylabel('$T_{12} (q^2)$')
 plt.legend()
-plt.gca().set_ylim([0,2])
+plt.gca().set_ylim([0,1.5])
 plt.show()
-'''
-for q in range(0,10):
-    plt.plot(q, FF(q, 'central')[0])
-    plt.show()
+

WC_SMcalculation.py
@@ -14,12 +14,12 @@ import scipy.integrate as integrate
 
 
 parameters = {'alpha' : 1/129,
-              'sin_theta_w**2' : 0.23,
+              'sin_theta_w**2' : 0.23124,
               'beta_0' : 23/3,
               'beta_1' : 116/3,
-              'Lambda' : 0.140,
-              'M_w' : 80,
-              'm_t' : 170,
+              'Lambda' : 0.225,
+              'M_w' : 80.41,
+              'm_t' : 173.8,
               'm_b' : 4.8,
               'm_c' : 1.4
 }
@@ -216,18 +216,18 @@ def WC1_6(mu): #returns a list with WC1 - WC6
     for j in range(len(WC_coefficients)):
         for i in range(len(coefficients['r'])):
             WC_coefficients[j] += coefficients['k'][j][i] *\
-                     eta(mu, 'LL')**(coefficients['a'][i])
+                     eta(mu, 'NLL')**(coefficients['a'][i])
     return WC_coefficients
 
 
 
 def WC7_eff(mu):
-    factor1 = eta(mu, 'LL')**(16/23) * C_7 + 8/3 *\
-              (eta(mu, 'LL')**(14/23)- eta(mu, 'LL')**(16/24)) * C_8
+    factor1 = eta(mu, 'NLL')**(16/23) * C_7 + 8/3 *\
+              (eta(mu, 'NLL')**(14/23)- eta(mu, 'NLL')**(16/24)) * C_8
     factor2 = 0
     for i in range(len(coefficients['r'])):
         factor2 += coefficients['h'][i] *\
-                   eta(mu, 'LL')**(coefficients['a'][i])
+                   eta(mu, 'NLL')**(coefficients['a'][i])
     return factor1 + factor2
 
 
@@ -266,3 +266,4 @@ def WC9_eff(mu, q):
     return factor1 + factor2 + factor3 + factor4
 
 
+print(WC10 * 2 * np.pi * 129)